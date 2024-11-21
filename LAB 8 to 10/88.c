677729f4a7b860bbc081f621b677a1650ea7ea81
Thanay Bodda <thanaybodda@gmail.com>
2024-11-21 13:09:10 +0530

    Create 88.c

---
+++ LAB 8 to 10/88.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+    
+int N;
+int A[N];
+int B[N];
+
+printf("Enter the Array Size: ");
+scanf("%d",&N);
+
+for (int i = 0; i < N; i++)
+{
+    printf("Enter A[%d]", i);
+    scanf("%d", &A[i]);
+}
+
+int *P = A;
+
+for (int i = 0; i < N; i++)
+{
+    B[i] = *(P +i);
+
+}
+
+for (int i = 0; i < N; i++)
+{
+    printf("%d", A[i]);
+}
+printf("\n");
+for (int i = 0; i < N; i++)
+{
+    printf("%d", B[i]);
+}
+ 
+    return 0;
+}
