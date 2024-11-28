fc1ca539d163439da89e3e4b133fe3cc81d23f7c
Thanay Bodda <thanaybodda@gmail.com>
2024-11-28 14:22:00 +0530

    Update findDigits.c

--- CASE STUDY/findDigits.c
+++ CASE STUDY/findDigits.c
@@ -4,30 +4,31 @@ int main()
 {
     int t;
     printf("Enter number b/w 0 and 15\n");
-    do{
-    printf("Enter the number of test cases: ");
-    scanf("%d",&t);// taking no of test cases to store the cases in array 
-    }while(t>15 || t<0);
-    int N[t];//for storing the cases
+    do {
+        printf("Enter the number of test cases: ");
+        scanf("%d", &t);
+    } while (t > 15 || t < 0);
+
+    int N[t];
     printf("Enter the values\n");
-    for(int i = 0;i<t;i++)
+    for (int i = 0; i < t; i++)
     {
-        printf("case%d : ",i + 1);
-        scanf("%d",&N[i]);
+        printf("case%d : ", i + 1);
+        scanf("%d", &N[i]);
     }
     
-    for(int j=0;j<t;j++)//for index of array representing case 
+    for (int j = 0; j < t; j++)
     {
-         int pos = 0;
-        for(int k = N[j];k>0;k/=10)//separating each digit and checking if divisible
+        int pos = 0;
+        for (int k = N[j]; k > 0; k /= 10)
         {
             int a = k % 10;
-            if(a != 0 && N[j] % a == 0)
+            if (a != 0 && N[j] % a == 0)
             {
                 pos++;
             }
         }
-        printf("no.of positions in case%d = %d\n",j+1,pos);
+        printf("no.of positions in case%d = %d\n", j + 1, pos);
     }
     
     return 0;
